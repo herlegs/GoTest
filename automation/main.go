a7c637a0565f27e7b6101d401d00d0dbf6605b5a
yuguang.xiao <yuguang.xiao@bytedance.com>
2021-01-20 19:50:38 +0800

    save

---
+++ automation/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+}
+
+func readCSV() []*PSMQPS {
+}
+
+type PSMQPS struct {
+	PSM string
+	AGW string
+	Cluster string
+	AppID int
+	// peak
+	AppQPS int
+}
--- playground/playground.go
+++ playground/playground.go
@@ -1,16 +1,102 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	decoded, err := base64.StdEncoding.DecodeString(base64Str)
-	if err != nil {
-		fmt.Printf("err:%v\n", err)
+	parts := strings.Fields(str)
+	res := ""
+	for i := 0; i < len(parts); i+=2 {
+		res += parts[i]+"\t"+parts[i+1]+"\n"
 	}
-	fmt.Printf("len:%v,%v\n", len(string(decoded)), string(decoded))
+	fmt.Printf("%v\n",res)
 }
 
-var base64Str = "eyJlbGVtZW50cyI6W3siYXR0cmlidXRlcyI6eyJlbWFpbCI6InRoZWEudGFuMDdAZ21haWwuY29tIiwiZnVsbE5hbWUiOiJNYXJpYSBSaXN0aGlhIEFwcGxlIFRhbiBDYcOxZXRlIn0sInR5cGUiOiJjdXN0b21lciIsImN1c3RvbWVyX2lkIjoiVTMxMjU0OTE5MjEifV0sInR5cGUiOiJ0cmFuc2l0aW9uIn0="
+func getProductFromPSM(psm string) string {
+	if parts := strings.Split(psm, "."); len(parts) == 3 {
+		return parts[0]
+	}
+	return ""
+}
+
+var str =`product
+psm
+toutiao
+toutiao.shield.http2thrift_demo
+life
+life.api.poly
+life
+life.api.feed
+life
+life.api.item
+maya
+maya.share.api
+toutiao
+toutiao.novel.channel_goapi
+toutiao
+toutiao.shield.agw_thrift_api_service
+h
+h.api.client
+ugc
+toutiao.ugc.vote_publish
+video
+toutiao.video.biz_service_praise
+ugc
+toutiao.ugc.publish_goapi
+life
+vinna.api.app
+internal
+toutiao.fe.magic
+temai
+cmp.ecom.ken
+toutiao
+toutiao.imtoutiao.ceremony
+h
+h.api.in_api
+ugc
+toutiao.top.author_growth
+toutiao
+ttgame.assistant.api
+toutiao
+toutiao.reading.ugcapi
+h
+h.api.query
+h
+h.api.ugc
+video
+toutiao.video.user_gateway
+video
+toutiao.video.tv_platform
+sandbox
+toutiao.shield.agw_thrift_api_service
+automobile
+motor.pgcapi.content
+h
+h.api.operation
+h
+h.api.solution
+hybrid
+em.mars.api
+h
+h.api.system
+h
+h.api.study
+h
+h.api.guide
+h
+h.api.study_room
+h
+h.fe.wechat_node
+h
+h.api.reach
+toutiao
+toutiao.user_action.report
+h
+h.api.play
+h
+h.api.user
+video
+toutiao.video.dante
+`
